5789f6a5e8a9e1978d422bab35aef051610abc85
laurenraddatz <lara8641@colorado.edu>
2016-04-23 19:06:54 -0600

    adding functions

---
+++ Tournament/include/Tournament.h
@@ -0,0 +1,39 @@
+#ifndef TOURNAMENT_H
+#define TOURNAMENT_H
+#include <string>
+
+
+struct Gods{
+	std::string name, attribute;
+	int health, attack, agility;
+
+	Gods(){};
+	Gods(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility)
+	{
+		name = in_name;
+		attribute = in_attribute;
+		health = in_health;
+		attack = in_attack;
+		agility = in_agility;
+	}
+
+};
+
+class Tournament
+{
+    public:
+        Tournament();
+        ~Tournament();
+        void InsertGod(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility);
+        Gods *Fight(Gods *a, Gods *b);
+        void printGods();
+        void findGod(std::string in_name);
+
+    protected:
+
+    private:
+        int godSize = 8;
+        Gods *godList[8];
+};
+
+#endif // TOURNAMENT_H
