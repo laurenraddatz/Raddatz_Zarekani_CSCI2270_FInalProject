b323fd5a990735b7e083269f0794b6a3a4632a87
persiaprince <lykuseternus@gmail.com>
2016-04-24 17:56:20 -0600

    Update Tournament.h

--- project/Tournament.h
+++ project/Tournament.h
@@ -6,7 +6,7 @@
 struct Gods{
 	std::string name, attribute;
 	int health, attack, agility;
-	Gods  *parent, *left, *right;
+	Gods* parent, *left, *right;
 
 	Gods(){};
 	Gods(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility)
@@ -30,13 +30,13 @@ class Tournament
         ~Tournament();
         void InsertGod(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility);
         Gods *Fight(Gods *a, Gods *b);
-        void printGods(int tier_choice);
+        void printGods(int choice);
         bool findGod(std::string in_name);
         void playerControl(Gods *a, Gods *b);
         void deleteGod(int index);
         void manualFight(Gods *a, Gods *b);
         void deleteAll(Gods *node);
-        void makeGod(std::string in_name, std::string in_attribute, std::string in_dominantStat);
+        void makeGod(std::string in_name, std::string in_attribute, std::string in_dominantStat, std::string in_inferiorStat);
         Gods* getRoot();
         void makeTree();
 
