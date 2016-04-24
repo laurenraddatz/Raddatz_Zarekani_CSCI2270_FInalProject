d5ea0c90c7401d6ca1ee2104a455d5e32fc589b9
persiaprince <lykuseternus@gmail.com>
2016-04-23 21:31:43 -0600

    added menu

--- project/main.cpp
+++ project/main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 
 int main()
 {
+    Tournament newTournament;
     string data;
-    string name, attribute, health, attack, agility;
+    string userInput, userInput2;
+    string name, attribute, health, attack, agility, dominant, inferior;
     ifstream inFile("gods.txt");
     while(getline(inFile, data)){
         stringstream SS(data);
@@ -18,9 +20,32 @@ int main()
         getline(SS, health, ',');
         getline(SS, attack, ',');
         getline(SS, agility, ',');
-        cout << name << attribute << health << attack << agility << endl;
+        newTournament.InsertGod(name, attribute, stoi(health), stoi(attack) , stoi(agility));
     }
     cout << "Welcome to the Divine Tournament" << endl;
     cout << "Immortal blood will be shed today" << endl;
+     while(userInput != "2"){
+        cout << "======Main Menu======" << endl;
+        cout << "1. Choose Your Champion" << endl;
+        cout << "2. Quit" << endl;
+        getline(cin, userInput);
+
+        if(userInput == "1"){
+            cout << "1. Choose a God" << endl;
+            cout << "2. Create a God" << endl;
+            getline(cin, userInput2);
+            if(userInput2 == "2"){
+                cout << "Choose Your God's Name" << endl;
+                getline(cin, name);
+                cout << "Choose Your God's Attribute" << endl;
+                getline(cin, attribute);
+                cout << "Choose Your God's dominant stat(attack, health, or agility)" << endl;
+                getline(cin, dominant);
+                cout << "Choose Your God's inferior stat(attack, health, or agility)" << endl;
+                getline(cin, inferior);
+                newTournament.makeGod(name, attribute, dominant, inferior);
+            }
+        }
+     }
     return 0;
 }
