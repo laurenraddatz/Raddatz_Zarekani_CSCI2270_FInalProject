b3cdb967d589ddbaf8551eb9bc5c60bad06c22eb
persiaprince <lykuseternus@gmail.com>
2016-04-23 20:28:28 -0600

    Update Tournament.cpp

--- project/Tournament.cpp
+++ project/Tournament.cpp
@@ -13,14 +13,37 @@ Tournament::Tournament(){
 Tournament::~Tournament(){
 
 }
-//Gods  Tournament::*Fight(Gods *a, Gods, *b){
+Gods* Tournament::Fight(Gods *a, Gods *b){
+    Gods fighterA = *a;
+    Gods fighterB = *b;
+    while(fighterA.health >= 0 and fighterB.health >= 0){ //both Gods still standing
+        modifierA = (rand() % 100)/100.00; //percentage
+        modifierB = (rand() % 100)/100.00; //percentage
+        if(fighterA.agility * modifierA < fighterB.agility * modifierB){ //whether the attack misses or hits
+            fighterA.health = fighterA.health - (fighterB.attack * modifierB);
+        }
+        if(fighterA.agility * modifierA < fighterB.agility * modifierB){ //whether the attack misses or hits
+            fighterB.health = fighterB.health - (fighterA.attack * modifierA);
+        }
 
-//}
+    }
+    if(fighterA.health > fighterB.health){
+        return a;
+    }else{
+        return b;
+    }
+}
 void Tournament::InsertGod(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility){
     Gods *newGod = new Gods(in_name, in_attribute, in_health, in_attack, in_agility);
-        for(int i = 0; i < godSize; i++){
-            if(godList[i] != NULL){
+        int initialI = rand() % 10;
+        for(int i = initialI; i < godSize + initialI ; i++){
+            if(godList[i] == NULL){
                 godList[i] = newGod;
+               // cout << godList[i]->name << " " << i << endl;
+                break;
+            }
+            if(i >= godSize){
+                i = 0;
             }
     }
 }
