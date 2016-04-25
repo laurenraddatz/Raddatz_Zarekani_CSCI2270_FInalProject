a93b2ea08c1cb1a4efa984770acb3a13538b9be3
persiaprince <lykuseternus@gmail.com>
2016-04-25 14:27:33 -0600

    a bunch of stuff
    changed the constructor, changed fight, added manual fight, added readFile,added InitializeTourny, tweaked runTournament

--- project/Tournament.cpp
+++ project/Tournament.cpp
@@ -2,13 +2,13 @@
 #include <string>
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <ctime> //for srand
 using namespace std;
 
 Tournament::Tournament(){
-    for(int i = 0; i < godSize; i++){
-        godList[i] = NULL;
-    }
+    InitializeTourny();
     srand(time(NULL)); //for random number generator
 
 }
@@ -23,13 +23,12 @@ Gods* Tournament::Fight(Gods *a, Gods *b){
     while(fighterA.health >= 0 and fighterB.health >= 0){ //both Gods still standing
         modifierA = (rand() % 100)/100.00; //percentage
         modifierB = (rand() % 100)/100.00; //percentage
-        if(fighterA.agility * modifierA < fighterB.agility * modifierB){ //whether the attack misses or hits
+        if(fighterB.agility * modifierB - fighterA.agility * modifierA > 5){ //whether the attack misses or hits
             fighterA.health = fighterA.health - (fighterB.attack * modifierB);
         }
-        if(fighterA.agility * modifierA < fighterB.agility * modifierB){ //whether the attack misses or hits
+        if(fighterA.agility * modifierA - fighterB.agility * modifierB > 5){ //whether the attack misses or hits
             fighterB.health = fighterB.health - (fighterA.attack * modifierA);
         }
-
     }
     if(fighterA.health > fighterB.health){
         return a;
@@ -194,12 +193,12 @@ void Tournament::printGods(int choice){
     }
     else if(choice == 2){
         for(int i = 0; i < 4; i++){
-            cout << godList2[i]->name << endl;
+    //        cout << godList2[i]->name << endl;
         }
     }
     else if(choice == 3){
         for(int i = 0; i < 2; i++){
-            cout << godList3[i]->name << endl;
+   //         cout << godList3[i]->name << endl;
         }
     }
 }
@@ -213,29 +212,105 @@ void Tournament::deleteAll(Gods *node){
     //cout<<"Deleting: "<<node->name<<endl;
     delete node;
 }
+void Tournament::readFile(){
+    string data, temp;
+    string name, attribute, health, attack, agility;
+    ifstream inFile("gods.txt");
+    while(getline(inFile, data)){
+        stringstream SS(data);
+        getline(SS, name, ',');
+        getline(SS, attribute, ',');
+        getline(SS, health, ',');
+        getline(SS, attack, ',');
+        getline(SS, agility, ',');
+        InsertGod(name, attribute, stoi(health), stoi(attack) , stoi(agility));
+    }
+}
+void Tournament::InitializeTourny(){
+    godSize = 8;
+    *godList = new Gods[godSize];
+    for(int i = 0; i < godSize; i++){
+        godList[i] = NULL;
+    }
+    readFile(); //reshuffle list
+}
+Gods* Tournament::manualFight(Gods *a, Gods *b){
+    cout << "Take Up Your Arms" << endl;
+    cout << "It's Time to Earn Your Divinity" << endl;
+    Gods playerChar = *a;
+    Gods fighterB = *b;
+    string userInput;
+    int turn = 0;
+    float playerModifierAgility, playerModifierAttack, playerModifierHealth, modifierB;
+    while(playerChar.health >= 0 and fighterB.health >= 0){ //both Gods still standing
+        userInput = "";
+        cout << "Turn " << turn << ":" << endl;
+        while(userInput != "attack" and userInput != "defense" and userInput != "speed" and userInput != "balanced"){
+            cout << "Will you focus on attack, defense, speed, or a balanced form?" << endl;
+            getline(cin, userInput);
+        }
+        if(userInput == "attack"){
+            playerModifierAttack = (rand() % 60 + 40)/100.00; //percentage
+            playerModifierAgility = (rand() % 80)/100.00;
+            playerModifierHealth = 0;
+        }else if(userInput == "defense"){
+            playerModifierAttack = 0; //percentage
+            playerModifierAgility = (rand() % 80)/100.00;
+            playerModifierHealth = (rand() % 40)/100.00;
+        }else if(userInput == "speed"){
+            playerModifierAttack = 0; //percentage
+            playerModifierAgility = (rand() % 60 + 40)/100.00;
+            playerModifierHealth = 0;
+        }else if(userInput == "balanced"){
+            playerModifierAttack = 0; //percentage
+            playerModifierAgility = (rand() % 100 + 10)/100.00;
+            playerModifierHealth = 0;
+        }
 
+        modifierB = (rand() % 100)/100.00; //percentage
+        cout << modifierB << " " << playerModifierAgility << endl;
+        if(fighterB.agility * modifierB - playerChar.agility * playerModifierAgility > 5){ //whether the attack misses or hits
+            playerChar.health = playerChar.health - ((fighterB.attack * modifierB) * (1.00 - playerModifierHealth));
+            cout << "You took " << ((fighterB.attack * modifierB) * (1.00 - playerModifierHealth)) << " damage" << endl;
+        }else{
+            cout << "Enemy Attack Missed!" << endl;
+        }
+        if(playerChar.agility * playerModifierAgility - fighterB.agility * modifierB > 5){ //whether the attack misses or hits
+            fighterB.health = fighterB.health - ((playerChar.attack * playerModifierAgility) * (1.00 + playerModifierAttack));
+            cout << "Enemy takes " << ((playerChar.attack * playerModifierAgility) * (1.00 + playerModifierAttack)) << " damage" << endl;
+        }else{
+            cout << "Your Attack Missed!" << endl;
+        }
+        turn++;
+    }
+    if(playerChar.health > fighterB.health){
+        return a;
+    }else{
+        cout << "Game Over" << endl;
+        return b;
+    }
+}
 void Tournament::runTournament(){
     int countMatch = 0;
     Gods * winner;
     Gods **godListTemp;
-    cout << "The Tournament Will Now Begin" << endl;
+    cout << "+++The Tournament Will Now Begin+++" << endl;
     for (int i = 0; i < godSize; i = i + 2){
         cout << godList[i]->name << " vs " << godList[i+1]->name << endl;
     }
-    for(int j = 0; j < 3; j++){
+    for(int j = 0; j < 3; j++){ //loop for each tournament tier
         godListTemp = new Gods*[godSize];
-        for (int i = 0; i < godSize; i++){
+        for (int i = 0; i < godSize; i++){ //copy godList
             godListTemp[i] = godList[i];
         }
-        godSize = godSize / 2;
+        godSize = godSize / 2; //half godSize for the next tier
         *godList = new Gods[godSize];
-        cout << "Round " << round << endl;
-        countMatch = 0;
-        for (int i = 0; i < godSize*2; i = i + 2){
+        cout << "+++Round " << round << endl;
+        countMatch = 0; //we start the following loop at match 0
+        for (int i = 0; i < godSize*2; i = i + 2){ //loops through each match of the tier
             winner = new Gods();
-            if(godListTemp[i] != player and godListTemp[i+1] != player){
-                *winner = *Fight(godListTemp[i],godListTemp[i+1]);
-                cout << winner << endl;
+            if(godListTemp[i] != player and godListTemp[i+1] != player){ //have the first 2 gods in the list fight
+                *winner = *Fight(godListTemp[i],godListTemp[i+1]); //create a clone of the winning God set to a new location
                 cout << godListTemp[i]->name << " vs " << godListTemp[i+1]->name << endl;
                 cout << winner->name << " is the winner" << endl;
                 winner->left = godListTemp[i];
@@ -244,9 +319,12 @@ void Tournament::runTournament(){
                 godListTemp[i+1]->parent = winner;
                 godList[countMatch] = winner;
             }else{
-                cout << "Player Match, to be filled later" << endl;
-                *winner = *Fight(godListTemp[i],godListTemp[i+1]);
                 cout << godListTemp[i]->name << " vs " << godListTemp[i+1]->name << endl;
+                if(godListTemp[i] == player){
+                    *winner = *manualFight(godListTemp[i],godListTemp[i+1]);
+                }else{
+                    *winner = *manualFight(godListTemp[i+1],godListTemp[i]);
+                }
                 cout << winner->name << " is the winner" << endl;
                 winner->left = godListTemp[i];
                 godListTemp[i]->parent = winner;
@@ -256,14 +334,8 @@ void Tournament::runTournament(){
             }
             countMatch++;
         }
-        for(int i = 0; i < godSize; i++){
-            cout << godList[i]->left->name << " look here" << endl;
-        }
         round++;
     }
-    Gods *temp = godList[0];
-    while(temp != NULL){
-    cout << temp->name << endl;
-    temp = temp->left;
-    }
+    root = godList[0];
+    InitializeTourny();
 }
