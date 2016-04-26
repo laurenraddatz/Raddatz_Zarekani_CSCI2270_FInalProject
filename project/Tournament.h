713afaf7ef93eb0677e53654460bf930eec96615
persiaprince <lykuseternus@gmail.com>
2016-04-26 15:14:59 -0600

    speech stuff

--- project/Tournament.h
+++ project/Tournament.h
@@ -4,18 +4,19 @@
 
 
 struct Gods{
-	std::string name, attribute;
+	std::string name, attribute, speech;
 	int health, attack, agility;
 	Gods  *parent, *left, *right;
 
 	Gods(){};
-	Gods(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility)
+	Gods(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility, std::string in_speech)
 	{
 		name = in_name;
 		attribute = in_attribute;
 		health = in_health;
 		attack = in_attack;
 		agility = in_agility;
+		speech = in_speech;
 		parent = NULL;
         	left = NULL;
         	right = NULL;
@@ -28,7 +29,7 @@ class Tournament
     public:
         Tournament();
         ~Tournament();
-        void InsertGod(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility);
+        void InsertGod(std::string in_name, std::string in_attribute, int in_health, int in_attack, int in_agility, std::string in_speech);
         Gods *Fight(Gods *a, Gods *b);
         void printGods(int tier_choice);
         bool findGod(std::string in_name);
@@ -36,7 +37,7 @@ class Tournament
         void deleteGod(int index);
         Gods* manualFight(Gods *a, Gods *b);
         void deleteAll(Gods *node);
-        void makeGod(std::string in_name, std::string in_attribute, std::string in_dominantStat, std::string in_inferiorStat);
+        void makeGod(std::string in_name, std::string in_attribute, std::string in_dominantStat, std::string in_inferiorStat, std::string in_speech);
         Gods* getRoot();
         void makeTree();
         void runTournament();
